3c71c293f266d01ffcb7784fbe11f933fbe6f823
Freeman <ejmorga1@gmail.com>
2018-08-19 18:55:08 -0400

    links to posts and users

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.User.findAll({}).then(function(dbUsers) {
+  app.get("/", function (req, res) {
+    db.User.findAll({}).then(function (dbUsers) {
       res.render("index", {
         msg: "Welcome!",
         users: dbUsers
@@ -12,26 +12,43 @@ module.exports = function(app) {
   });
 
   // Load user page and pass in an user by id
-  app.get("/user/:id", function(req, res) {
-    db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
+  app.get("/user/:id", function (req, res) {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbUser) {
       res.render("user", {
         user: dbUser
       });
     });
   });
 
-    // Load example page and pass in an example by id
-    app.get("/login/:id", function(req, res) {
-      db.dbSignUp.findOne({ where: { id: req.params.id } }).then(function(dbSignUp) {
-        res.render("login", {
-          example: dbSignUp
-        });
+  // Load example page and pass in an example by id
+  app.get("/login/:id", function (req, res) {
+    db.dbSignUp.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbSignUp) {
+      res.render("login", {
+        example: dbSignUp
       });
     });
-  
+  });
+
+  // Load index page
+  app.get("/posts", function (req, res) {
+    db.User.findAll({}).then(function (dbUsers) {
+      res.render("index", {
+        msg: "Welcome!",
+        users: dbUsers
+      });
+    });
+  });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
